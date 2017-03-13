89680da127e353da0e9b3352c68615ca5136afaa
DeepikaPadam <manideepika.padam@gmail.com>
2017-03-13 16:55:01 +0530

    Validations Demo

---
+++ Validations/js/app.js
@@ -0,0 +1,21 @@
+/**
+ * Created by mdeepika on 13/3/17.
+ */
+
+(function(){
+    angular.module('myApp').controller('validationController', function(){
+        var vm = this;
+        vm.name="";
+        vm.email="";
+
+        vm.submitForm = function(){
+            console.log("hdgj");
+            if(vm.name=="" || vm.email==""){
+                vm.submitted = true;
+            }else if(vm.name!="" && vm.email!=""){
+                alert("Submitted successfully");
+            }
+        };
+    });
+})();
+
---
+++ Validations/js/module.js
@@ -0,0 +1,7 @@
+/**
+ * Created by mdeepika on 13/3/17.
+ */
+
+(function(){
+    angular.module('myApp',['ngMessages']);
+})();
