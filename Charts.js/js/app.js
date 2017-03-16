113912bff45dc0fd78d72bef34bb5bb31df293cc
DeepikaPadam <manideepika.padam@gmail.com>
2017-03-16 17:07:39 +0530

    Linear graph using chart.js

---
+++ Charts.js/js/app.js
@@ -0,0 +1,32 @@
+/**
+ * Created by mdeepika on 16/3/17.
+ */
+(function(){
+    angular.module("myApp",['chart.js']).controller("graphController", function(){
+        var vm = this;
+        vm.labels = ["January", "February", "March", "April", "May", "June", "July"];
+        vm.data = [
+            [65, 59, 80, 81, 56, 55, 40],
+            [28, 48, 40, 19, 86, 27, 90]
+        ];
+        vm.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+        vm.options = {
+            scales: {
+                yAxes: [
+                    {
+                        id: 'y-axis-1',
+                        type: 'linear',
+                        display: true,
+                        position: 'left'
+                    },
+                    {
+                        id: 'y-axis-2',
+                        type: 'linear',
+                        display: true,
+                        position: 'right'
+                    }
+                ]
+            }
+        };
+    });
+})();
---
+++ Charts.js/js/module.js
@@ -0,0 +1,7 @@
+/**
+ * Created by mdeepika on 16/3/17.
+ */
+
+(function(){
+    angular.module("myApp", ['chart.js']);
+})();
